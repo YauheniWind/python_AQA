198ea38ad1fc26616cd702d96522e9e926a84de9
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-02-06 20:28:10 +0100

    attempt to use value data types

---
+++ pr_1.py
@@ -0,0 +1,16 @@
+full_name = "John Snow" # String
+age = 29 # Int
+
+print(f"Name: {full_name}, age: {age}")
+
+str = "Hello"
+int_1 = 1
+float_1 = 2.0
+list_1 = [1, 2]
+dict_1 = {"1": 1}
+tuple_1 = (1, 2)
+set_1 = {1, 2, 3, 4}
+bool_1 = True
+
+print(str, int_1, float_1, list_1, dict_1, tuple_1, set_1, bool_1)
+
