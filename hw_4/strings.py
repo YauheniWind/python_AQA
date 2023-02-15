da837018e6062817bbadb747cbe09d5294e22bce
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-02-15 13:33:21 +0100

    redesigned to a function

--- hw_4/strings.py
+++ hw_4/strings.py
@@ -1,28 +1,30 @@
 password = "HelloWorld"
 
-list_of_char = list(password)
-first_char = list_of_char.pop(0)
-last_char = list_of_char.pop()
-third_element = list_of_char.pop(2)
-reversed_third_element = list_of_char.pop(-2)
 
-print(first_char)
-print(last_char)
-print(third_element)
-print(reversed_third_element)
-print(len(list_of_char))
+def change_string(string):
+    list_of_char = list(string)
+    list_of_char.pop(0)
+    list_of_char.pop()
+    list_of_char.pop(2)
+    list_of_char.pop(-2)
+    return len(list_of_char)
 
-long_string = "HelloMyNameIsEv"
-print(long_string[:8])
-print(long_string[4:8])
-multiples = ""
-for index in range(len(long_string)):
-    if index % 3 == 0:
-        print(index)
-        multiples += long_string[index]
-print(multiples)
 
-print(long_string[::-1])
+string_len = change_string(password)
+print(string_len)
+
+long_string_var = "HelloMyNameIsEv"
+
+
+def long_string(string):
+    multiples_in_func = ""
+    for index in range(len(string)):
+        if index % 3 == 0:
+            multiples_in_func += string[index]
+    return string[:8], string[4:8], multiples_in_func, string[::-1]
+
+
+print(long_string(long_string_var))
 
 
 def name_in_string(name):
@@ -34,17 +36,19 @@ def name_in_string(name):
 
 print(name_in_string("Ev"))
 
-
 test_tring = "Hello world!"
-find_w = test_tring.find("w")
-letters = test_tring.split(" ")
-join_letters = "".join(letters)
 
-print(find_w)
-print(len(join_letters))
 
-start_with = test_tring.startswith("Hello")
-end_with = test_tring.endswith("qwe")
+def trying(string):
+    letters = string.split(" ")
+    join_string = "".join(letters)
+    return (
+        string.find("w"),
+        len(join_string),
+        string.startswith("Hello"),
+        string.endswith("qwe"),
+    )
 
-print(start_with)
-print(end_with)
+
+hola = trying(test_tring)
+print(hola)
