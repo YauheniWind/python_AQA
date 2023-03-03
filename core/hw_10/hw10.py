57f7821da69bdad732f29f5aa764b7d5af8dcd3d
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-03-03 12:22:25 +0100

    made hw10

---
+++ core/hw_10/hw10.py
@@ -0,0 +1,73 @@
+class Flower:
+    def __init__(self, name: str, cost: int, deadline: int):
+        self.name = name
+        self.cost = cost
+        self.deadline = deadline
+
+    def show_info(self):
+        print(
+            f"It is {self.name}, cost is {self.cost}$, deadline ~ {self.deadline} days"
+        )
+
+
+class Rose(Flower):
+    def __init__(self, name: str, cost: int, deadline: int):
+        super().__init__(name, cost, deadline)
+
+
+class Sunflower(Flower):
+    def __init__(self, name: str, cost: int, deadline: int):
+        super().__init__(name, cost, deadline)
+
+
+class Bouquet:
+    def __init__(self, list_of_flower):
+        self.list_of_flower = list_of_flower
+
+    def deadline_flower(self):
+        deadline_list = []
+        for i in self.list_of_flower:
+            deadline_list.append(i.deadline)
+
+        res_deadline = sum(deadline_list) // len(deadline_list)
+        print(f"The bouquet will fade in {res_deadline} days")
+        return res_deadline
+
+    def sort_cost(self):
+        sorted_cost = []
+        for i in self.list_of_flower:
+            sorted_cost.append(i.cost)
+        res_cost = sorted(sorted_cost)
+        print(f"The bouquet sorted from min to max {res_cost}")
+        return res_cost
+
+    def find_flower(self, f):
+        if f in self.list_of_flower:
+            print(f"Flower {f} is in bouquet")
+            return True
+        else:
+            print(f"Flower {f} is not in bouquet")
+            return False
+
+    def total(self):
+        total_cost = []
+        for i in self.list_of_flower:
+            total_cost.append(i.cost)
+        res_total = sum(total_cost)
+        print(f"Total cost per bouquet {res_total}$ ")
+        return res_total
+
+
+rose = Rose("Rose", 5, 7)
+sunflower = Sunflower("Sunflower", 2, 10)
+rose.show_info()
+sunflower.show_info()
+
+flowers = [rose, sunflower]
+
+bouquet = Bouquet(flowers)
+
+bouquet.deadline_flower()
+bouquet.sort_cost()
+bouquet.find_flower("Sunflower")
+bouquet.total()
